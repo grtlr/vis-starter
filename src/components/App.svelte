4c9942eae2665718a27b1971e4a50a6ca7c36726
Jochen Görtler <jochen.goertler@uni-konstanz.de>
2019-09-19 14:35:55 +0200

    Better example

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,11 +1,24 @@
 <script>
-	export let name;
+    import * as d3 from '../vendor/d3-bundle.js';
+
+    const width = 400;
+    const height = 400;
+
+    const xs = [1, 2, 2.5];
+    const ys = [3, 2, 1];
+
+    const x = d3.scaleLinear()
+        .domain([0, 4])
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, 4])
+        .range([height, 0]);
+
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<svg {width} {height}>
+    {#each d3.zip(xs, ys) as l}
+    <line x1={x(1)} y1={y(l[0])} x2={x(3)} y2={y(l[1])} stroke="black" />
+    {/each}
+</svg>
